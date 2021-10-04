b9207f06a2ed1737c61a57972f29befb5f31732f
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-03 22:26:21 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.onload = ev => {
     btnSend.addEventListener("click", (e) => {
-        let email = email.value()
+        let email = gmail.value()
 
         fetch("http://codingstep.freevar.com/", {
             method: "post",
@@ -8,3 +8,35 @@ window.onload = ev => {
         })
     })
 }
+
+
+var mandrill = require('node-mandrill') ('<your API Key>');
+
+function sendEmail ( _name, _email, _subject, _message) {
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: _email , name: _name}],
+            from_email: 'noreply@yourdomain.com',
+            subject: _subject,
+            text: _message
+        }
+    }, function(error, response){
+        if (error) console.log( error );
+
+        else console.log(response);
+    });
+}
+
+// define sua própria API de e-mail que aponta para o seu servidor.
+
+app.post( '/api/sendemail/', function(req, res){
+
+    let _name = req.body.name;
+    let _email = req.body.email;
+    let _subject = req.body.subject;
+    let _message = req.body.message;
+
+// implemente sua proteção ou verificações de spam
+
+    sendEmail ( _name, _email, _subject, _message );
+});
